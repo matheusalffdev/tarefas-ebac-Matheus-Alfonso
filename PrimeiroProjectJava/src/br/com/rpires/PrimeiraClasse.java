228bb432cb440657d6b84d7b9cf386d574d559e9
Matheus Alfonso <matheus.alfonso92@gmail.com>
2023-08-17 21:16:35 -0300

    Exercicio Modulo 4 EBAC

---
+++ PrimeiroProjectJava/src/br/com/rpires/PrimeiraClasse.java
@@ -0,0 +1,15 @@
+package br.com.rpires;
+
+public class PrimeiraClasse {
+
+	public static void main(String[] args) 
+	{
+		String ambiente = args[0];
+		
+		if(ambiente.equals("DEV"))
+		{
+			System.out.print("DEVv");			
+		}
+	}
+
+}
