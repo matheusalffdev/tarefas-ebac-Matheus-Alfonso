0320d3b846d418f94701b869ee7b5c95c61d9408
Matheus Alfonso <matheus.alfonso92@gmail.com>
2023-08-22 11:29:53 -0300

    Adicionado exercicio modulo 8

---
+++ mod8/mod8.java
@@ -0,0 +1,17 @@
+public class mod8 {
+    public static void main(String args[])
+    {
+        calcularMedia();
+    }
+
+    private static void calcularMedia()
+    {
+        float num1 = 5;
+        float num2 = 4;
+        float num3 = 3;
+        float num4 = 8;
+
+        float media = ((num1 + num2 + num3 + num4)/4);
+        System.out.println(media);
+    }
+}
