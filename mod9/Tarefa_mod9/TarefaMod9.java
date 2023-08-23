e66e62d6b8a020c5b491dfb8ecfd8052f7143edf
Matheus Alfonso <matheus.alfonso92@gmail.com>
2023-08-23 12:05:03 -0300

    Adicionado exercicio modulo 9

---
+++ mod9/Tarefa_mod9/TarefaMod9.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+public class TarefaMod9
+{
+    public static void main(String[] args)
+    {
+        Scanner ler = new Scanner(System.in);
+
+        System.out.println("Informe um valor inteiro: ");
+        int num = Integer.parseInt(ler.nextLine());
+
+        System.out.println("O numero digitado foi: " + num);
+    }
+}
