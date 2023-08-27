81e51ab449763960d76712ff6e8cda793a5dfcb1
Matheus Alfonso <matheus.alfonso92@gmail.com>
2023-08-27 17:43:08 -0300

    Adicionado exercicio modulo 10

---
+++ mod10/Tarefa_controle_de_fluxos/Tarefa_Mod10.java
@@ -0,0 +1,45 @@
+import java.util.Scanner;
+
+public class Tarefa_Mod10
+{
+        public static void main(String[] args)
+        {
+                Scanner scanner = new Scanner(System.in);
+
+                System.out.println("Informe a primeira nota:");
+                float nota1 = scanner.nextFloat();
+                System.out.println("Informe a segunda nota: ");
+                float nota2 = scanner.nextFloat();
+                System.out.println("Informe a terceira nota: ");
+                float nota3 = scanner.nextFloat();
+                System.out.println("Informe a quarta nota: ");
+                float nota4 = scanner.nextFloat();
+
+                float media = calculaMedia(nota1, nota2, nota3, nota4);
+
+                verificaAprovacao(media);
+        }
+
+        public static float calculaMedia(float n1, float n2, float n3, float n4)
+        {
+                float media = (n1+n2+n3+n4)/4;
+
+                return media;
+        }
+
+        public static void verificaAprovacao(float media)
+        {
+                if(media >= 7)
+                {
+                        System.out.println("Aluno aprovado!");
+                }
+                else if (media >= 5)
+                {
+                        System.out.println("Aluno de recuperacao!");
+                }
+                else
+                {
+                        System.out.println("Aluno reprovado!");
+                }
+        }
+}
